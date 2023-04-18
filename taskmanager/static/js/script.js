cbf62829d70450e1dc8eb433a24b08e02dfa957f
Sergii Kostanets <sergiikostanets@gmail.com>
2023-04-18 10:50:22 +0100

    Deleting Categories

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -3,4 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-  });
+    // modal initialization
+    let modal = document.querySelectorAll('.modal');
+    M.Modal.init(modal);
+});
